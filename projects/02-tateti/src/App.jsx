e15641e713fc05ad22535d1e100e6d6526e66799
kshi <jcdominguez01.jcd@gmail.com>
2024-07-30 01:32:21 -0300

    tictactoe advanced

---
+++ projects/02-tateti/src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+import {useState} from "react" 
+const Turns ={
+  X:'x',
+  O: 'o',
+}
+
+
+const Square = ({children,updateBoard,index}) => {
+  return(
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
+function App() {
+  const [board,setBoard] = useState(Array(9).fill(null))
+
+  return (
+    <>
+      <main className='board'>
+        <h1>Tic Tac Toe</h1>
+        <section className='game'>
+          {
+          board.map((_, index) => {
+            return(
+              <Square 
+                key={index} 
+                index={index} 
+                >
+                  {index}
+                </Square>
+              
+            )
+          }
+          )}
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
